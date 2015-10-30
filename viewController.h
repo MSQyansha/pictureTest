0a363be3e9df610e1785f7820d8fa50a8ebc3f5a
MSQyansha <1224894560@qq.com>
2015-10-30 16:50:17 +0800

    Create viewController.h

---
+++ viewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  pictureTest
+//
+//  Created by 007 on 15/10/30.
+//  Copyright © 2015年 闫莎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+{
+    UIImageView *_zoomImageView;
+    UIImageView *_circleView;
+    UILabel *_textLabel;
+}
+@property(nonatomic, strong)UITableView *listTableView;
+@end
+
